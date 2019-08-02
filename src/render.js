9fe72d514f71b4897b65f70b0af0a2a2c0c69fa0
Seán mór Clarke <sean.clarke@guardian.co.uk>
2019-08-02 15:25:06 +0100

    with basic page framework in place

--- src/render.js
+++ src/render.js
@@ -1,4 +1,6 @@
 import templateHTML from "./src/templates/main.html!text"
+import mustache from 'mustache'
+import axios from 'axios'
 
 const team = process.argv.find(a => a.includes("--")).toString().replace("--","")
 
@@ -9,16 +11,20 @@ const dataurls = [
 },
 {
     team : "eng",
-    data: "https://interactive.guim.co.uk/docsdata-test/1HEkkUzrVloX1sPXYWkoz_8-L86rcH-WRJgh0EWnegFo.json"
+    data: "https://interactive.guim.co.uk/docsdata-test/1kyqSBA0SaP6T2JC8WrpoCdNumKXUPvw2LxnF10OqgfE.json"
 }
 ]
 
 export async function render() {
 
-    console.log(team)
+    var teamurl = dataurls.find(d => d.team == team).data;
+
+    var data = (await axios.get(teamurl)).data;
+
+    var output = mustache.render(templateHTML,data)
 
     // this function just has to return a string of HTML
     // you can generate this using js, e.g. using Mustache.js
 
-    return templateHTML;
+    return output;
 }
